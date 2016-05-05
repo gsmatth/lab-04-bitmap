480179d6630d69a545b8bbcc551ce76facd52ab1
FazioD <faziod@uw.edu>
2016-05-05 16:15:58 -0700

    adding test scaffoling

---
+++ bitmap-team-deuces/test/read-write-test.js
@@ -0,0 +1,19 @@
+'use strict';
+const fs = require('fs');
+const expect = require('chai').expect;
+const readWrite = require('read-write');
+
+
+
+//Buff variable is anything//
+describe('read-write file', function(){
+  it ('create a buffer', function() {
+    expect(Buff).not.to.be.null;
+  });
+});
+
+//place holder for testing. Add in what we will be testing//
+describe('transform module', function() {
+  it('should be parsing the bitmap data', function(){
+    expect(bitmap.?).to.eql.(?)
+  }

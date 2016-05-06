e826a0ba39be14458e35900a4afae05b9e5d1380
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2016-05-05 20:31:49 -0700

    transform module is complete

--- bitmap-team-deuces/index.js
+++ bitmap-team-deuces/index.js
@@ -4,10 +4,21 @@ const transform = require('./lib/transform');
 const fs = require('fs');
 
 fs.readFile(__dirname + '/img/bitmap1.bmp', function(err, data) {
-  console.log(data.toString('utf8', 0, 2));
-  console.log(data.readInt16LE(2));
+  //console.log(data.toString('utf8', 0, 2));
+  console.log(data.slice(54, 58));
+  console.log(data.slice(58, 62));
+  console.log(data.slice(62, 66));
+  console.log(data.slice(66, 70));
+  console.log(data.slice(70, 74));
+  console.log(data.slice(74, 78));
+  console.log(data.slice(78, 82));
 
-  const pixelOffset = data.readInt32LE(10);
-  const pixelArray = data.slice(pixelOffset);
-  console.log(pixelArray[pixelOffset, 4]);
+  let prevIndex = 54;
+  for (var i = 58; i < 1071; i += 4) {
+    const b = data.slice(prevIndex, i);
+    console.log(b);
+
+    prevIndex = i;
+  }
+
 });
--- bitmap-team-deuces/lib/transform.js
+++ bitmap-team-deuces/lib/transform.js
@@ -1,9 +1,17 @@
 'use strict';
 
 module.exports = (function() {
-  const invert = function(bitmapObj, callback) {
-    let transformedBitmap;
-    return callback(transformedBitmap);
+  const invert = function(colorTableBuf, callback) {
+    let prevIndex = 0;
+    for (var i = 4; i < 1024; i += 4){
+      const b = colorTableBuf.slice(prevIndex, i);
+
+      for (var color in b.values()) {
+        color = 255 - color;
+      }
+      prevIndex = i;
+    }
+    return colorTableBuf;
   };
 
   return {
